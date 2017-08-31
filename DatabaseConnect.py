b72678c909f83f45be78894698a7af884f05e902
OMAsphyxiate <omasphyxiate@gmail.com>
2017-08-31 17:04:06 -0500

    Update

--- DatabaseConnect.py
+++ DatabaseConnect.py
@@ -1,14 +1,18 @@
 import pyodbc, psycopg2, sys, sqlanydb
 #sys.path.insert(0, 'C:/Users/Christian/Desktop/GitHub/')
-sys.path.insert(0, 'C:/Users/Reaper/Desktop/GitHub/')
+#sys.path.insert(0, 'C:/Users/Reaper/Desktop/GitHub/')
 import Connect as ct
 
-class DatabaseSync:
-	def __init__(self,query): #Initialze method
-		self.query = query
+#class DatabaseSync:
+#	def __init__(noquery=None): #Initialze method
+#		if sql is None:
+#			noquery = "SELECT 'No Query'"
+#		else:
+#			noquery = sql
+noquery = "SELECT 'No Query'"
 		
-	def ESGrab(host,query=None):
-	query = query or self.query #Use class query if no alternative
+def ESGrab(host,query=None):
+	query = query or noquery #Use class query if no alternative
 	try:
 		esconn = sqlanydb.connect( #Establish Connection
 			UID=ct.SAUID,
@@ -26,51 +30,68 @@ class DatabaseSync:
 		print("Could not connect to HOST: %s" % host)
 		#If connect fails, return host IP
 
-	def PGSelect(query=None):
-		query = query or self.query #Use class query if no alternative
-		pgconn = psycopg2.connect( #Establish Connection to DB
-			"dbname=%s user=%s host=%s password=%s" %
-			ct.PGDatabase,ct.PGUID,ct.PGHost,ct.PGPWD)
-		pgcurs = pgconn.cursor() #Create Cursor
+def PGSelect(query=None):
+	query = query or noquery
+	pgconn = psycopg2.connect( #Establish Connection to DB
+		"dbname=%s user=%s host=%s password=%s" % 
+		(ct.PGDatabase,ct.PGUID,ct.PGHost,ct.PGPWD))
+	pgcurs = pgconn.cursor() #Create Cursor
+	pgcurs.execute(query) #Execute query argument
+	pgresults = pgcurs.fetchall() #Fetch query results
+	pgcurs.close() #Close Cursor
+	pgconn.close() #Close Connection
+	return pgresults #Return stored results
+
+def PGInsert(query):
+	query = query or noquery #Use class query if no alternative
+	pgconn = psycopg2.connect( #Establish Connection to DB
+		"dbname=%s user=%s host=%s password=%s" %
+		(ct.PGDatabase,ct.PGUID,ct.PGHost,ct.PGPWD))
+	pgcurs = pgconn.cursor() #Create Cursor
+	try: #Try INSERT Query
 		pgcurs.execute(query) #Execute query argument
-		pgresults = pgcurs.fetchall() #Fetch query results
-		pgcurs.close() #Close Cursor
-		pgconn.close() #Close Connection
-		return pgresults #Return stored results
+		pgconn.commit() #Commit INSERT
+		#return print('INSERT Commited to Database')
+	except:
+		return print('Unable to execute INSERT query: %s' % query)
+	pgcurs.close() #Close Cursor
+	pgconn.close() #Close Connection
 
-	def PGInsert(query=None):
-		query = query or self.query #Use class query if no alternative
-		pgconn = psycopg2.connect( #Establish Connection to DB
-			"dbname=%s user=%s host=%s password=%s" %
-			ct.PGDatabase,ct.PGUID,ct.PGHost,ct.PGPWD)
-		pgcurs = pgconn.cursor() #Create Cursor
-		try: #Try INSERT Query
-			pgcurs.execute(query) #Execute query argument
-		except:
-			print('Unable to execute INSERT query')
-		pgcurs.close() #Close Cursor
-		pgconn.close() #Close Connection
+def PGInsertMany(query,list):
+	query = query or noquery #Use class query if no alternative
+	pgconn = psycopg2.connect( #Establish Connection to DB
+		"dbname=%s user=%s host=%s password=%s" %
+		(ct.PGDatabase,ct.PGUID,ct.PGHost,ct.PGPWD))
+	pgcurs = pgconn.cursor() #Create Cursor
+	try: #Try INSERT Query
+		pgcurs.executemany(query,) #Execute query argument
+		pgconn.commit() #Commit INSERT
+		#return print('INSERT Commited to Database')
+	except:
+		return print('Unable to execute INSERT query: %s' % query)
+	pgcurs.close() #Close Cursor
+	pgconn.close() #Close Connection
 
-	def MSSelect(query=None):
-		query = query or self.query #Use class query if no alternative
-		msconn = pyodbc.connect( #Establish Connection with MS Database
-			"Driver={%s};Server=%s;Database=%s;UID=%s;PWD=%s;" % 
-			ct.MSDriver,ct.MSServer,ct.MSDatabase,ct.MSUID,ct.MSPWD)
-		mscurs = msconn.cursor() #Create Cursor
-		mscurse.execute(query) #Execute Query
-		msresults = mscurs.fetchall() #Fetch query results
-		mscurs.close() #Close Cursor
-		msconn.close() #Close Connection
+def MSSelect(query=None):
+	query = query or noquery #Use class query if no alternative
+	msconn = pyodbc.connect( #Establish Connection with MS Database
+		"Driver={%s};Server=%s;Database=%s;UID=%s;PWD=%s;" % 
+		(ct.MSDriver,ct.MSServer,ct.MSDatabase,ct.MSUID,ct.MSPWD))
+	mscurs = msconn.cursor() #Create Cursor
+	mscurse.execute(query) #Execute Query
+	msresults = mscurs.fetchall() #Fetch query results
+	mscurs.close() #Close Cursor
+	msconn.close() #Close Connection
 
-	def MSInsert(query=None):
-		query = query or self.query #Use class query if no alternative
-		msconn = pyodbc.connect( #Establish Connection with MS Database
-			"Driver={%s};Server=%s;Database=%s;UID=%s;PWD=%s;" % 
-			ct.MSDriver,ct.MSServer,ct.MSDatabase,ct.MSUID,ct.MSPWD)
-		mscurs = msconn.cursor() #Create Cursor
-		try:
-			mscurse.execute(query) #Execute INSERT Query
-		except:
-			print('Unable to execute INSERT query')
-		mscurs.close() #Close Cursor
-		msconn.close() #Close Connection
+def MSInsert(query=None):
+	query = query or noquery #Use class query if no alternative
+	msconn = pyodbc.connect( #Establish Connection with MS Database
+		"Driver={%s};Server=%s;Database=%s;UID=%s;PWD=%s;" % 
+		(ct.MSDriver,ct.MSServer,ct.MSDatabase,ct.MSUID,ct.MSPWD))
+	mscurs = msconn.cursor() #Create Cursor
+	try:
+		mscurse.execute(query) #Execute INSERT Query
+	except:
+		print('Unable to execute INSERT query')
+	mscurs.close() #Close Cursor
+	msconn.close() #Close Connection
